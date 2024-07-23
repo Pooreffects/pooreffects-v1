b1207cebd171db650cb7a43c375bb7ebac9272ca
Mohamed Benabdellah <77970397+Pooreffects@users.noreply.github.com>
2024-07-23 02:25:54 +0100

    feat: installed skillIcons and animated the marquee

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,15 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      animation: {
+        "loop-scroll": "loop-scroll 20s linear infinite",
+      },
+      keyframes: {
+        "loop-scroll": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-50%)" },
+        },
+      },
       fontFamily: {
         sans: [
           "Bricolage Grotesque Variable",
