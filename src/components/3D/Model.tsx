fcb247f14c842cd47f66111274232571a4612743
Mohamed Benabdellah <77970397+Pooreffects@users.noreply.github.com>
2024-07-26 19:00:09 +0100

    feat: loaded and animated GLTF Model on Hero and did some performance optimization

--- src/components/3D/Model.tsx
+++ src/components/3D/Model.tsx
@@ -1,6 +1,5 @@
-// Model.tsx
-import React, { useRef } from "react";
-import { useLoader } from "@react-three/fiber";
+import { forwardRef, useState, useEffect } from "react";
+import { useFrame, useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { Group } from "three";
 
@@ -8,20 +7,37 @@ interface ModelProps {
   url: string;
 }
 
-const Model: React.FC<ModelProps> = ({ url }) => {
-  // Load the GLTF model
+const Model = forwardRef<Group, ModelProps>(({ url }, ref: any) => {
   const gltf = useLoader(GLTFLoader, url);
+  const [rotationDirection, setRotationDirection] = useState(1); // 1 for clockwise, -1 for counter-clockwise
+  const rotationLimit = 0.1745; // 10 degrees in radians
 
-  // Create a ref for the model
-  const modelRef = useRef<Group>(null);
+  useEffect(() => {
+    const handleRotation = () => {
+      if (ref.current) {
+        if (ref.current.rotation.y >= rotationLimit) {
+          setRotationDirection(-1);
+        } else if (ref.current.rotation.y <= -rotationLimit) {
+          setRotationDirection(1);
+        }
+      }
+    };
 
-  // If you need to perform operations on the model, you can do so here
+    const interval = setInterval(handleRotation, 100); // Check rotation direction every 100ms
+    return () => clearInterval(interval);
+  }, [ref]);
+
+  useFrame(() => {
+    if (ref.current) {
+      ref.current.rotation.y += 0.001 * rotationDirection; // Adjust the speed of rotation here
+    }
+  });
 
   return (
-    <group ref={modelRef}>
+    <group ref={ref}>
       <primitive object={gltf.scene} />
     </group>
   );
-};
+});
 
 export default Model;
--- src/components/3D/Scene.tsx
+++ src/components/3D/Scene.tsx
@@ -1,18 +1,23 @@
+import { useRef } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import Model from "./Model";
+import { Group } from "three";
 
 const Scene: React.FC = () => {
+  const modelRef = useRef<Group>(null);
+
   return (
-    <div className="w-full h-80 items-center">
+    <div className="w-full h-full items-center overflow-hidden">
       <Canvas style={{ width: "100%", height: "100%" }}>
-        <PerspectiveCamera makeDefault position={[15, 30, 65]} fov={75} />
-        <Model url="/scene.gltf" />
+        <PerspectiveCamera makeDefault position={[30, 40, 65]} fov={75} />
+        <ambientLight intensity={1.7} color={"#f0fdfa"} />
+        <Model ref={modelRef} url="/scene.gltf" />
         <OrbitControls enableZoom={false} maxDistance={80} minDistance={80} />
-        <ambientLight intensity={1.7} color={"#f0fdfa"}/>
       </Canvas>
     </div>
   );
 };
 
 export default Scene;
+
