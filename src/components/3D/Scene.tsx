9d4dccf10c294ae3ce99979d13d8946e183ef585
Mohamed Benabdellah <77970397+Pooreffects@users.noreply.github.com>
2024-07-27 23:40:36 +0100

    feat: loaded a better GLTF Model, reformulated the Hero section with basic CTA

--- src/components/3D/Scene.tsx
+++ src/components/3D/Scene.tsx
@@ -10,15 +10,15 @@ const Scene: React.FC = () => {
   return (
     <div className="w-full h-full absolute">
       <Canvas style={{ width: "100%", height: "100%", display: "flex" }}>
-        <PerspectiveCamera makeDefault position={[10, 20, 40]} fov={14} />
-        <ambientLight intensity={1.7} color={"#f0fdfa"} />
-        <Model ref={modelRef} url="/scene.gltf" position={[0, -5, 0]} />
+        <PerspectiveCamera makeDefault position={[10, 20, 40]} fov={1.8} />
+        <ambientLight intensity={10} color={"#f0fdfa"} />
+        <Model ref={modelRef} url="/scene.gltf" position={[0, 0, 0]} />
         <Sparkles
           count={4000}
           size={25}
           speed={1}
           opacity={1}
-          scale={200}
+          scale={30}
           color="#14b8a6"
         />
         <OrbitControls enableZoom={false} maxDistance={80} minDistance={80} />
