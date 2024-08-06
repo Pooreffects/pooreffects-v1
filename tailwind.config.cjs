cbe9ff286cc21360eb660db151ce2ee6cb818a7b
Mohamed Benabdellah <77970397+Pooreffects@users.noreply.github.com>
2024-08-06 14:46:59 +0100

    feat: custom scrollbar, global CSS, and tweak the theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,22 +6,22 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-      transitionTimingFunction: {
-        "pooreffects-ease": "cubic-bezier(0.645, 0.045, 0.355, 1)",
-      },
       colors: {
         indigo: {
-          100: "rgb(224 231 255)",
-          900: "rgb(49 46 129)",
+          100: "#e0e7ff",
+          900: "#312e81",
         },
         teal: {
-          500: "rgb(20 184 166)",
+          500: "#14b8a6",
         },
         pink: {
-          300: "rgb(249 168 212)",
+          300: "#f9a8d4",
         },
         "bg-light": "linear-gradient(40deg, #edf2f7, #f9a8d4)",
         "bg-dark": "linear-gradient(40deg, #080a0f, #001120)",
+      },
+      transitionTimingFunction: {
+        "pooreffects-ease": "cubic-bezier(0.645, 0.045, 0.355, 1)",
       },
       animation: {
         "loop-scroll": "loop-scroll 20s linear infinite",
@@ -37,19 +37,16 @@ module.exports = {
           to: { transform: "translateX(-50%)" },
         },
         moveInCircle: {
-          "0%": { transform: "rotate(0deg)" },
+          "0%, 100%": { transform: "rotate(0deg)" },
           "50%": { transform: "rotate(180deg)" },
-          "100%": { transform: "rotate(360deg)" },
         },
         moveVertical: {
-          "0%": { transform: "translateY(-50%)" },
+          "0%, 100%": { transform: "translateY(-50%)" },
           "50%": { transform: "translateY(50%)" },
-          "100%": { transform: "translateY(-50%)" },
         },
         moveHorizontal: {
-          "0%": { transform: "translateX(-50%) translateY(-10%)" },
+          "0%, 100%": { transform: "translateX(-50%) translateY(-10%)" },
           "50%": { transform: "translateX(50%) translateY(10%)" },
-          "100%": { transform: "translateX(-50%) translateY(-10%)" },
         },
       },
       fontFamily: {
